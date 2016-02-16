0144d93675d1d9eddc73f00186a3d788cb023c90
omeke <tlegen_omir@mail.ru>
2016-02-16 23:44:08 +0600

    news template v1

---
+++ frontend/modules/temp/views/default/temp.php
@@ -0,0 +1,101 @@
+<div class="container">
+    <div class="col-md-12">
+        <h1 class="page-header text-center">
+            Лента новостей
+            <small>Category Name</small>
+        </h1>
+    </div>
+
+    <div class="col-md-3">
+
+        <div class="panel panel-default">
+            <!-- Default panel contents -->
+            <div class="panel-heading">Популярные посты</div>
+            <div class="list-group">
+                <a href="#" class="list-group-item">Порядок проведения общего собрания</a>
+                <a href="#" class="list-group-item">Основные понятия</a>
+                <a href="#" class="list-group-item">ЖКХ (Жилищно-коммунальное хозяйство)</a>
+                <a href="#" class="list-group-item">Права и обязанности жильцов</a>
+            </div>
+        </div>
+
+        <div class="panel panel-default">
+            <!-- Default panel contents -->
+            <div class="panel-heading">Последие добавленные отчеты</div>
+            <ul class="list-group">
+                <li class="list-group-item">Буревестник - квартал 1</li>
+                <li class="list-group-item">Аспан - квартал 2</li>
+                <li class="list-group-item">Верный - квартал 3</li>
+                <li class="list-group-item">Волна - квартал 1</li>
+                <li class="list-group-item">Домостройтельная - квартал 2</li>
+            </ul>
+        </div>
+
+    </div>
+
+    <div class="col-md-9">
+        <div class="col-md-6">
+            <a href="http://akyl.kz/page/show/name/poryadok"><img class="img-responsive" src="/img/sm_full.gif" alt=""></a>
+            <a href="http://akyl.kz/page/show/name/poryadok"><b>Порядок проведения общего собрания</b></a>
+
+            <p>Согласно нормам, установленным ст.ст. 42-1, 42-2 Закона РК «О жилищных отношениях» от 16.04.1997г.,
+                Вашему вниманию представлена следующая схема, иллюстрирующая составляющие, связанные с основными
+                вопросами проведения общего собрания собственников квартир (далее - ОССК) ...</p>
+
+            <div class="pull-left">
+                <small>14 февр. / 11:06</small>
+            </div>
+            <div class="pull-right">
+                <small><span class="glyphicon glyphicon-eye-open"></span>131</small>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <a href="http://akyl.kz/page/show/name/osn_p"><img class="img-responsive"
+                                                               src="img/concepts.gif" alt=""></a>
+            <a href="http://akyl.kz/page/show/name/osn_p"><b>Основные понятия</b></a>
+
+            <p>коммунальные услуги, кондоминиум, объект кондоминиума, собрание участников объекта кондоминиума,
+                орган управления объектом кондоминиума, надлежащее содержание объекта кондоминиума,
+                переоборудование, перепланировка, общее имущество, нежилое помещение ...</p>
+
+            <div class="pull-left">
+                <small>14 февр. / 11:06</small>
+            </div>
+            <div class="pull-right">
+                <small><span class="glyphicon glyphicon-eye-open"></span>131</small>
+            </div>
+        </div>
+        <div class="col-md-12">
+            <hr/>
+        </div>
+        <div class="col-md-6">
+            <a href="http://akyl.kz/page/show/name/jkx"><img class="img-responsive" src="/img/K_ch_k2.jpg" alt=""></a>
+            <a href="http://akyl.kz/page/show/name/jkx"><b>ЖКХ (Жилищно-коммунальное хозяйство)</b></a>
+
+            <p>На сегодняшний день жителей практически всех домов волнует множество вопросов по деятельности КСК и
+                управляющих компаний. В частности: ...</p>
+
+            <div class="pull-left">
+                <small>14 февр. / 11:06</small>
+            </div>
+            <div class="pull-right">
+                <small><span class="glyphicon glyphicon-eye-open"></span>131</small>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <a href="http://akyl.kz/page/show/name/pravo_j"><img class="img-responsive" src="/img/3.png" alt=""></a>
+            <a href="http://akyl.kz/page/show/name/pravo_j"><b>Права и обязанности жильцов</b></a>
+
+            <p> Каждый собственник жилья имеет права и обязанности, предоставленные действующим законодательством,
+                регулирующим жилищно-эксплуатационные отношения...</p>
+
+            <div class="pull-left">
+                <small>14 февр. / 11:06</small>
+            </div>
+            <div class="pull-right">
+                <small><span class="glyphicon glyphicon-eye-open"></span>131</small>
+            </div>
+        </div>
+
+    </div>
+</div>
